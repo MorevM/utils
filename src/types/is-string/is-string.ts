1a16164cf32fcd0f7cf6b7d6cb087526052d2286
MorevM <max.seainside@gmail.com>
2022-02-23 16:04:27 +0300

    fix(types): Clarify `isString` return type

--- src/types/is-string/is-string.ts
+++ src/types/is-string/is-string.ts
@@ -5,5 +5,5 @@
  *
  * @returns   {boolean}
  */
-export const isString = (value: any): boolean =>
+export const isString = (value: any): value is string =>
 	Object.prototype.toString.call(value) === '[object String]';
