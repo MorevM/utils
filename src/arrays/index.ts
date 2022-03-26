fc91d82eba6d9a7f0c1c745c8f190d3cc4706bac
MorevM <max.seainside@gmail.com>
2022-03-26 18:14:24 +0300

    fix(arrays): Add forgotten `array-unique` export

--- src/arrays/index.ts
+++ src/arrays/index.ts
@@ -1,3 +1,4 @@
+export * from './array-unique/array-unique';
 export * from './arrays-difference/arrays-difference';
 export * from './arrays-intersection/arrays-intersection';
 export * from './arrays-union/arrays-union';
