b85acf31436b6051012d5e4c78479cc136df5aa7
MorevM <max.seainside@gmail.com>
2022-04-11 17:11:43 +0300

    feat(browser): Add `supportsPassive` helper

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -4,3 +4,4 @@ export * from './is-mobile/is-mobile';
 export * from './is-motionless/is-motionless';
 export * from './is-osx/is-osx';
 export * from './is-touch/is-touch';
+export * from './supports-passive/supports-passive';
---
+++ src/browser/supports-passive/supports-passive.ts
@@ -0,0 +1,25 @@
+/**
+ * Checks whether the current browser supports passive event listeners.
+ *
+ * @returns   {boolean}
+ */
+export const supportsPassive = () => {
+	let supports = false;
+
+	const options = Object.defineProperty({}, 'passive', {
+		get() {
+			supports = true;
+			return null;
+		},
+	});
+
+	try {
+		const noop = () => {};
+		window.addEventListener('test:passive', noop, options);
+		window.removeEventListener('test:passive', noop, options);
+	} catch {
+		// no catch
+	}
+
+	return supports;
+};
