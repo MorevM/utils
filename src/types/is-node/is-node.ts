f46067bab9d8e780abcb7c755308fb251468d1d7
MorevM <max.seainside@gmail.com>
2022-04-13 20:27:18 +0300

    refactor: Add deprecation flag to `isNode` function due to incorrect name

--- src/types/is-node/is-node.ts
+++ src/types/is-node/is-node.ts
@@ -3,6 +3,8 @@
  *
  * @param     {any}       value   The value being evaluated.
  *
+ * @deprecated    This function will change in the next major release, use `isElement` instead.
+ *
  * @returns   {boolean}
  */
 export const isNode = (value: any): boolean =>
