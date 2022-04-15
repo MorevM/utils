190c02f2591c1c5426a9ef53f48445702a31b77a
MorevM <max.seainside@gmail.com>
2022-04-15 16:04:50 +0300

    feat(dom): Add `parent` argument to `getElementOffset`

--- src/dom/get-element-offset/get-element-offset.ts
+++ src/dom/get-element-offset/get-element-offset.ts
@@ -3,14 +3,34 @@ import { getWindowScroll } from '../get-window-scroll/get-window-scroll';
 type Axis = 'x' | 'y' | 'both';
 type ToReturn<T> = T extends 'both' ? { top: number; left: number } : number;
 
-export const getElementOffset = <T extends Axis = 'y'>(
-	el: HTMLElement, axis: T,
+const getElementScroll = <T extends Axis>(el: Element, axis: T): ToReturn<T> => {
+	if (axis === 'both') {
+		return { top: el.scrollTop, left: el.scrollLeft } as ToReturn<T>;
+	}
+
+	return axis === 'x' ? el.scrollLeft as ToReturn<T> : el.scrollTop as ToReturn<T>;
+};
+
+export const getElementOffset = <T extends Axis>(
+	el: Element,
+	axis: T,
+	parent: Element | Window = window,
 ): ToReturn<T> => {
-	const { top, left } = el.getBoundingClientRect();
-	const scroll = getWindowScroll(axis);
+	const parentRect = parent === window
+		? { top: 0, left: 0 }
+		: (parent as Element).getBoundingClientRect();
+	const elRect = el.getBoundingClientRect();
+
+	const top = elRect.top - parentRect.top;
+	const left = elRect.left - parentRect.left;
+
+	const scroll = parent === window
+		? getWindowScroll(axis)
+		: getElementScroll(parent as Element, axis);
 
 	if (axis === 'y') return top + (scroll as number) as ToReturn<T>;
 	if (axis === 'x') return left + (scroll as number) as ToReturn<T>;
+
 	return {
 		// @ts-expect-error -- TODO
 		top: top + (scroll.top as number),
