8c89b2c306dfc6af79204687a5c7bc877c95d82f
MorevM <max.seainside@gmail.com>
2022-05-22 00:43:59 +0300

    test: Fix `array-shuffle` test

--- src/arrays/array-shuffle/array-shuffle.test.ts
+++ src/arrays/array-shuffle/array-shuffle.test.ts
@@ -1,12 +1,19 @@
+/* eslint-disable @typescript-eslint/require-array-sort-compare */
 import { arrayShuffle } from './array-shuffle';
 
+const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
 describe('array-shuffle', () => {
+	// @TODO: Should test 1000 times
 	it('Returns the shuffled array', () => {
-		expect(arrayShuffle([1, 2, 3, 4])).not.toStrictEqual([1, 2, 3, 4]);
-		expect(arrayShuffle([1, 2])).not.toStrictEqual([1, 2]);
+		expect(arrayShuffle(input)).not.toStrictEqual([...input]);
 	});
 
-	it('Returns array as is if it contains single element', () => {
+	it('Contains the same elements after shuffle', () => {
+		expect(arrayShuffle(input).sort()).toStrictEqual([...input].sort());
+	});
+
+	it('Returns given array as is if it contains single element', () => {
 		expect(arrayShuffle([1])).toStrictEqual([1]);
 	});
 
