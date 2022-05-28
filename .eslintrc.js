3aca1c0ba34fb17c2cb9351470237849674f5726
MorevM <max.seainside@gmail.com>
2022-05-28 21:48:40 +0300

    chore: Disable some eslint rules for jsdoc
    JSDoc types not needed using TS

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,8 @@ module.exports = {
 	extends: ['@morev/eslint-config/preset/common'],
 	rules: {
 		'jsdoc/sort-tags': 'off', // @TODO: [2023-05-11] -- "see" should be under description mostly
+		// No need with TS
+		'jsdoc/require-param-type': 'off',
+		'jsdoc/require-returns-type': 'off',
 	},
 };
