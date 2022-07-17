04d003b8343f34a05b2689c8b23101061bef0e56
MorevM <max.seainside@gmail.com>
2022-07-17 22:37:16 +0300

    refactor!: Remove `isObjectsEqualJson` helper
    BREAKING CHANGE: Helper `isObjectsEqualJson` no longer exports. Use `deepEqual` instead.

--- src/objects/index.ts
+++ src/objects/index.ts
@@ -1,6 +1,5 @@
 export * from './deep-equal/deep-equal';
 export * from './form-data-to-object/form-data-to-object';
-export * from './is-objects-equal-json/is-objects-equal-json';
 export * from './object-keys-case/object-keys-case';
 export * from './omit/omit';
 export * from './pick/pick';
