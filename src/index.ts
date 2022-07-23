74ea9e473b8df8b6493bb719c01d1f53b27db965
MorevM <max.seainside@gmail.com>
2022-07-23 08:38:36 +0300

    refactor!: Move `format-bytes` to category `numbers`
    BREAKING CHANGE: Category `miscellaneous` no longer exists

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ export * from './dom';
 export * from './environment';
 export * from './functions';
 export * from './guards';
-export * from './miscellaneous';
 export * from './numbers';
 export * from './objects';
 export * from './strings';
