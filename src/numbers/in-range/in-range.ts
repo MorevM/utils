ccd756717924d8361a1ab5d1fc12ca45b6cc24cb
MorevM <max.seainside@gmail.com>
2022-09-25 23:38:52 +0300

    refactor(numbers): Deprecate `inRange` utility
    Use `rangeIncludes` instead

--- src/numbers/in-range/in-range.ts
+++ src/numbers/in-range/in-range.ts
@@ -1,7 +1,20 @@
 import { isNullish } from '../../guards';
+import type { ArrayOf } from '../../types';
 
-// TODO: Conditional tuple
-export const inRange = (value: number, ...ranges: Array<[number | null, number | null]>) =>
+type RangePart = number | null;
+
+/**
+ * Checks whether at least one of provided ranges includes a given number.
+ *
+ * @param   value    The number to search in ranges.
+ * @param   ranges   The ranges to check for occurrences.
+ *
+ * @returns            Is any provided range includes a given number.
+ *
+ * @deprecated  This function will be removed in the next major release for better consistency across the groups.
+ * 							Use `rangeIncludes` from `ranges` group instead.
+ */
+export const inRange = (value: number, ...ranges: Array<ArrayOf<'exactly', 2, RangePart>>) =>
 	ranges.some(([min, max]) => {
 		if (!isNullish(min) && value < min) return false;
 		// eslint-disable-next-line sonarjs/prefer-single-boolean-return
