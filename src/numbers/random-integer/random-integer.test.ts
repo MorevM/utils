7a8dfa3aaa3f65387b0e4250ce4181e342ce539c
MorevM <max.seainside@gmail.com>
2022-10-27 13:15:05 +0300

    fix(numbers): Set arguments of `randomInteger` as optional

--- src/numbers/random-integer/random-integer.test.ts
+++ src/numbers/random-integer/random-integer.test.ts
@@ -1,6 +1,13 @@
 import { randomInteger } from './random-integer';
 
 describe('random-integer', () => {
+	it('Returns the random integer with no arguments', () => {
+		const result = randomInteger();
+
+		expect(typeof result).toBe('number');
+		expect(result).not.toBeNaN();
+	});
+
 	it('Returns the random integer between a given minimum and maximum value (including)', () => {
 		const min = 10;
 		const max = 100;
--- src/numbers/random-integer/random-integer.ts
+++ src/numbers/random-integer/random-integer.ts
@@ -1,10 +1,12 @@
 /**
- * Generates a random integer (including `min` and `max` values).
+ * Generates a random integer (including `min` and `max` values). \
+ * Returns a random integer between 1 and `MAX_SAFE_INTEGER`
+ * if called with no arguments which covers the most frequent use case.
  *
  * @param   min   The lowest value to return.
  * @param   max   The highest value to return.
  *
  * @returns         Random integer value greater than `min` and less than `max` (inclusive)
  */
-export const randomInteger = (min: number, max: number): number =>
+export const randomInteger = (min: number = 1, max: number = Number.MAX_SAFE_INTEGER): number =>
 	Math.floor(Math.random() * (max - min + 1)) + min;
