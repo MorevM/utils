2716c3ef0e73876eda7da55dd1f9d1d96b3f01cb
MorevM <max.seainside@gmail.com>
2022-10-29 17:25:23 +0300

    feat(numbers): Allow to use `clamp` with only lower / upper bound

--- src/numbers/clamp/clamp.test.ts
+++ src/numbers/clamp/clamp.test.ts
@@ -16,4 +16,17 @@ describe('clamp', () => {
 	it('Works correctly if all given values are the same', () => {
 		expect(clamp(2, 2, 2)).toBe(2);
 	});
+
+	it('Works correctly if `min` and `max` arguments are omitted', () => {
+		expect(clamp(1)).toBe(1);
+		expect(clamp(Infinity)).toBe(Infinity);
+	});
+
+	it('Works correctly if the `max` argument is omitted', () => {
+		expect(clamp(10, 20)).toBe(20);
+	});
+
+	it('Works correctly if the `min` argument is omitted', () => {
+		expect(clamp(30, null, 20)).toBe(20);
+	});
 });
--- src/numbers/clamp/clamp.ts
+++ src/numbers/clamp/clamp.ts
@@ -2,10 +2,17 @@
  * Clamps a value between an upper and lower bound.
  *
  * @param   value   The preferred value to return.
- * @param   min     The lowest value to return.
- * @param   max     The highest value to return.
+ * @param   min     The lowest value to return or `null` to skip.
+ * @param   max     The highest value to return or `null` to skip.
  *
  * @returns         Value not less than the `min` and not greater than the `max`
  */
-export const clamp = (value: number, min: number, max: number): number =>
-	Math.max(min, Math.min(value, max));
+export const clamp = (
+	value: number,
+	min: number | null = -Infinity,
+	max: number | null = Infinity,
+) =>
+	Math.max(
+		min ?? -Infinity,
+		Math.min(value, max ?? Infinity),
+	);
