9b3b6b184ec1ac131e02a3120aa614dceb35ddbb
MorevM <max.seainside@gmail.com>
2022-11-20 13:22:56 +0300

    fix(types): ObjectValues returns an Array of values

--- src/types/objects/object-values/object-values.ts
+++ src/types/objects/object-values/object-values.ts
@@ -3,4 +3,4 @@ import type { PlainObject } from '../plain-object/plain-object';
 /**
  * Object values as an union with optional ability to set which keys should be presented.
  */
-export type ObjectValues<T extends PlainObject, K extends keyof T = keyof T> = T[K];
+export type ObjectValues<T extends PlainObject, K extends keyof T = keyof T> = Array<T[K]>;
