deb30b1dd84e6c8ad49a41523465ebbd28bd56ce
MorevM <max.seainside@gmail.com>
2022-11-27 19:52:37 +0300

    chore: Take account of updated shared ESLint config

--- src/strings/random-string/random-string.ts
+++ src/strings/random-string/random-string.ts
@@ -8,6 +8,8 @@
  * @returns                  Generated string of a given length.
  */
 export const randomString = (length: number = 10, startWithLatin: boolean = true): string => {
+	// TODO: Remove when issue is resolved: https://github.com/sindresorhus/eslint-plugin-unicorn/issues/1994
+	// eslint-disable-next-line unicorn/no-useless-spread
 	const base = [...new Array(length)].map(() => (Math.trunc(Math.random() * 36)).toString(36)).join('');
 	if (!startWithLatin) return base;
 	const latinCharacters = 'abcdefghijklmnopqrstuvwxyz';
