9b37f94073769a74cd4e6be3e7164c12d3d37ed6
MorevM <max.seainside@gmail.com>
2022-12-19 22:20:51 +0300

    feat(strings): Utility `unquote` should handle backticks as well

--- src/strings/unquote/unquote.test.ts
+++ src/strings/unquote/unquote.test.ts
@@ -1,14 +1,15 @@
 import { unquote } from './unquote';
 
 describe('unquote', () => {
-	it('Returns string as is if there is no quotes', () => {
+	it('Returns string as is if there are no quotes', () => {
 		expect(unquote('')).toBe('');
 		expect(unquote('test')).toBe('test');
 	});
 
-	it('Returns string without quotes', () => {
+	it('Returns string without quotes and backticks', () => {
 		expect(unquote('"test"')).toBe('test');
 		expect(unquote("'test'")).toBe('test');
+		expect(unquote('`test`')).toBe('test');
 	});
 
 	it('Returns string as is if different quotes used', () => {
--- src/strings/unquote/unquote.ts
+++ src/strings/unquote/unquote.ts
@@ -3,13 +3,13 @@
  *
  * @param   str   String to unquote.
  *
- * @returns       String without start/end quotes.
+ * @returns       String without start/end quotes/backticks.
  */
 export const unquote = (str: string): string => {
 	if (!str) return '';
 	const [first, last] = [str[0], str[str.length - 1]];
 	if (first !== last) return str;
 
-	if (first === '"' || first === "'") return str.slice(1, -1);
+	if (first === '"' || first === "'" || first === '`') return str.slice(1, -1);
 	return str;
 };
