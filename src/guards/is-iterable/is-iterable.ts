2e04df35bd1459f20397c56ebb6e8b2502d9e683
MorevM <max.seainside@gmail.com>
2022-12-25 00:49:41 +0300

    docs: JSDoc for `isIterable` utility

--- src/guards/is-iterable/is-iterable.ts
+++ src/guards/is-iterable/is-iterable.ts
@@ -1,2 +1,10 @@
-// eslint-disable-next-line unicorn/new-for-builtins
-export const isIterable = (value: any) => Symbol.iterator in Object(value);
+/**
+ * Checks whether a given value is iterable.
+ *
+ * @param   value   The value being evaluated.
+ *
+ * @returns         Whether the value is `Iterable`.
+ */
+export const isIterable = (value: any): value is Iterable<unknown> =>
+	// eslint-disable-next-line unicorn/new-for-builtins
+	Symbol.iterator in Object(value);
