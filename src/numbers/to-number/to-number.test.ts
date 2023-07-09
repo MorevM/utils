e3de98364e7a6dda1b29928e28157a621575b6f5
MorevM <max.seainside@gmail.com>
2023-07-09 04:47:17 +0300

    test(numbers): Add more tests for `to-number` utility

--- src/numbers/to-number/to-number.test.ts
+++ src/numbers/to-number/to-number.test.ts
@@ -1,6 +1,15 @@
 import { toNumber } from './to-number';
 
 describe('to-number', () => {
+	it('Returns a real numbers as is', () => {
+		expect(toNumber(0)).toBe(0);
+		expect(toNumber(-0)).toBe(-0);
+		expect(toNumber(-Infinity)).toBe(-Infinity);
+		expect(toNumber(+Infinity)).toBe(+Infinity);
+		expect(toNumber(1.11)).toBe(1.11);
+		expect(toNumber(Math.PI)).toBe(Math.PI);
+	});
+
 	it('Converts given number in string form to a real number', () => {
 		expect(toNumber('123')).toBe(123);
 		expect(toNumber('123.123')).toBe(123.123);
@@ -12,6 +21,7 @@ describe('to-number', () => {
 	it('Throws if it\'s impossible to cast to a number and no fallback value is provided', () => {
 		expect(() => toNumber('!')).toThrow();
 		expect(() => toNumber(['123'])).toThrow();
+		expect(() => toNumber(NaN)).toThrow();
 		expect(() => toNumber(null)).toThrow();
 		expect(() => toNumber(undefined)).toThrow();
 		expect(() => toNumber({})).toThrow();
