1f3cc7afddbba5a57c24cbcf980db5bedab2f807
MorevM <max.seainside@gmail.com>
2023-07-13 04:57:30 +0300

    fix(guards): Make `isEmpty` more type-safe

--- src/guards/is-empty/is-empty.ts
+++ src/guards/is-empty/is-empty.ts
@@ -5,13 +5,20 @@
  *
  * @returns         Whether the value is `empty`
  */
+import type { PlainObject } from '../../types';
 import { isIterable } from '../is-iterable/is-iterable';
 import { isObject } from '../is-object/is-object';
 
-export const isEmpty = (value: any): boolean => {
-	if ([undefined, null, false, NaN, 0, ''].includes(value)) {
-		return true;
-	}
+type Empty = undefined | null | false | 0 | 0n | '' | readonly [] | [] | PlainObject<never>;
+
+export const isEmpty = (value: unknown): value is Empty => {
+	if (typeof (value) === 'undefined') return true;
+	if (value === null) return true;
+	if (value === false) return true;
+	if (Number.isNaN(value)) return true;
+	if (value === 0) return true;
+	if (value === 0n) return true;
+	if (value === '') return true;
 
 	if (isIterable(value) && typeof value === 'object' && 'length' in value && value.length === 0) return true;
 	if (isIterable(value) && typeof value === 'object' && 'size' in value && value.size === 0) return true;
