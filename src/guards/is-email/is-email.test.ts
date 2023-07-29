2ae7d656b78a619244ed0aa752f886e265d77337
MorevM <max.seainside@gmail.com>
2023-07-30 01:13:54 +0300

    fix(guards): Make `isEmail` utility more type-safe

--- src/guards/is-email/is-email.test.ts
+++ src/guards/is-email/is-email.test.ts
@@ -1,6 +1,18 @@
 import { isEmail } from './is-email';
 
 describe('is-email', () => {
+	it('Returns `false` for any non-string input', () => {
+		expect(isEmail(null)).toBe(false);
+		expect(isEmail(undefined)).toBe(false);
+		expect(isEmail(1)).toBe(false);
+		expect(isEmail(true)).toBe(false);
+		expect(isEmail({})).toBe(false);
+		expect(isEmail([])).toBe(false);
+		expect(isEmail(new Date())).toBe(false);
+		expect(isEmail(new Set())).toBe(false);
+		expect(isEmail(new Map())).toBe(false);
+	});
+
 	it('Returns `true` if correct email given', () => {
 		expect(isEmail('test@test.te')).toBe(true);
 		expect(isEmail('test.test@test.te')).toBe(true);
--- src/guards/is-email/is-email.ts
+++ src/guards/is-email/is-email.ts
@@ -1,11 +1,12 @@
 /* eslint-disable regexp/no-unused-capturing-group, no-autofix/regexp/no-unused-capturing-group  -- It's better here for "readability" :D */
+import { isString } from '../is-string/is-string';
+
 /**
  * Checks whether a given string is a valid email.
  *
- * @param   str   The value being evaluated.
+ * @param   value   The value being evaluated.
  *
- * @returns       Whether the value is a valid email address
+ * @returns         Whether the value is a valid email address.
  */
-export const isEmail = (str: string): boolean =>
-	// eslint-disable-next-line unicorn/no-unsafe-regex
-	/^(([^\s"(),.:;<>@[\\\]]+(\.[^\s"(),.:;<>@[\\\]]+)*)|(".+"))@((\[(?:\d{1,3}\.){3}\d{1,3}])|(([\d\-A-Za-zЁА-яё]+\.)+[A-Za-zЁА-яё]{2,}))$/.test(str);
+export const isEmail = (value: unknown): value is string =>
+	isString(value) && /^(([^\s"(),.:;<>@[\\\]]+(\.[^\s"(),.:;<>@[\\\]]+)*)|(".+"))@((\[(?:\d{1,3}\.){3}\d{1,3}])|(([\d\-A-Za-zЁА-яё]+\.)+[A-Za-zЁА-яё]{2,}))$/.test(value);
