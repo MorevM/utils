07b615b2ae6ae6e4327463f27811e4f4e6bf1f0f
MorevM <max.seainside@gmail.com>
2024-01-05 01:28:09 +0300

    test(guards): Clarify `isDate` test

--- src/guards/is-date/is-date.test.ts
+++ src/guards/is-date/is-date.test.ts
@@ -1,11 +1,13 @@
 import { isDate } from './is-date';
 
 describe('is-date', () => {
-	it('Returns `true` if a given value is a `Date` object', () => {
+	it('Returns `true` if a given value is a `Date` object (even invalid)', () => {
 		expect(isDate(new Date())).toBe(true);
+		expect(isDate(new Date('foo'))).toBe(true);
 	});
 
 	it('Returns `false` if a given value is not a `Date` object', () => {
 		expect(isDate({})).toBe(false);
+		expect(isDate('2011-05-27')).toBe(false);
 	});
 });
