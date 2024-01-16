e12aeca859bde0b4a6e5011d7ccf04a4fe93138f
MorevM <max.seainside@gmail.com>
2024-01-17 00:47:47 +0300

    chore: Remove outdated ESLint rules from `.eslintrc.cjs`

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,17 +1,5 @@
 module.exports = {
 	root: true,
 	extends: ['@morev/eslint-config/preset/common'],
-	rules: {
-		'jsdoc/sort-tags': 'off', // @TODO: [2023-05-11] -- "see" should be under description mostly
-		'prefer-object-has-own': 'off',
-		'@typescript-eslint/no-unnecessary-condition': 'off',
-	},
-	overrides: [
-		{
-			files: '.github/**.*',
-			rules: {
-				'yml/file-extension': 'off',
-			},
-		},
-	],
+	rules: {},
 };
