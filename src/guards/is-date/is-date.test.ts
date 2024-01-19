cdf27351b352349536cc4f407689657bbf000424
MorevM <max.seainside@gmail.com>
2024-01-19 18:57:18 +0300

    feat(dates): Add second argument to `isDate` utility to check validity

--- src/guards/is-date/is-date.test.ts
+++ src/guards/is-date/is-date.test.ts
@@ -10,4 +10,12 @@ describe('is-date', () => {
 		expect(isDate({})).toBe(false);
 		expect(isDate('2011-05-27')).toBe(false);
 	});
+
+	it('Returns `false` if a given value is not a valid Date object using second argument', () => {
+		expect(isDate(new Date('foo'), true)).toBe(false);
+	});
+
+	it('Returns `true` if a given value is a valid Date object using second argument', () => {
+		expect(isDate(new Date(), true)).toBe(true);
+	});
 });
--- src/guards/is-date/is-date.ts
+++ src/guards/is-date/is-date.ts
@@ -1,9 +1,18 @@
 /**
  * Checks whether a given value is a date object.
+ * Optionally it can also check whether the given value represents a valid date.
  *
- * @param   value   The value being evaluated.
+ * @param   value           The value being evaluated.
+ * @param   checkValidity   Whether to return `false` if the Date object is invalid, e.g. `new Date('foo')`.
  *
- * @returns         Whether the value is Date object
+ * @returns                 Whether the value is Date object. \
+ *                          If the second argument is `true` it returns `false` for invalid Date objects.
  */
-export const isDate = (value: unknown): value is Date =>
-	Object.prototype.toString.call(value).includes('Date');
+export const isDate = (value: unknown, checkValidity?: boolean): value is Date => {
+	const isDateObject = Object.prototype.toString.call(value).includes('Date');
+	if (!isDateObject) return false;
+
+	if (!checkValidity) return isDateObject;
+
+	return !Number.isNaN((value as Date).getTime());
+};
