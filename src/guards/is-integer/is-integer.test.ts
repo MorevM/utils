5b5ee789104606c96dcb70062b405ca2cef41749
MorevM <max.seainside@gmail.com>
2024-01-21 15:54:10 +0300

    test: Clarify `isInteger` tests

--- src/guards/is-integer/is-integer.test.ts
+++ src/guards/is-integer/is-integer.test.ts
@@ -3,10 +3,19 @@ import { isInteger } from './is-integer';
 describe('is-integer', () => {
 	it('Returns `true` if a given value is an integer number', () => {
 		expect(isInteger(1)).toBe(true);
+		expect(isInteger(0)).toBe(true);
+		expect(isInteger(-1)).toBe(true);
+		expect(isInteger(Number.MAX_SAFE_INTEGER)).toBe(true);
+		expect(isInteger(Number.MAX_SAFE_INTEGER + 1)).toBe(true);
 	});
 
 	it('Returns `false` if a given value is a fractional number', () => {
 		expect(isInteger(1.2)).toBe(false);
+	});
+
+	it('Returns `false` if a given value is `Infinity`', () => {
+		expect(isInteger(Infinity)).toBe(false);
+		expect(isInteger(-Infinity)).toBe(false);
 	});
 
 	it('Returns `false` if a given value is `NaN`', () => {
