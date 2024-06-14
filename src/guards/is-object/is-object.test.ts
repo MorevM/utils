0f6a800a3caed12455264ecd69cc480902bc1edf
MorevM <max.seainside@gmail.com>
2024-06-14 16:09:04 +0300

    test(guards): Ensure `isObject` is `false` for `File` object

--- src/guards/is-object/is-object.test.ts
+++ src/guards/is-object/is-object.test.ts
@@ -8,6 +8,7 @@ describe('is-object', () => {
 	it('Returns `false` if a given value is object of some type other than `Object`', () => {
 		expect(isObject(new Date())).toBe(false);
 		expect(isObject(new Blob())).toBe(false);
+		expect(isObject(new File([], ''))).toBe(false);
 		expect(isObject(new FormData())).toBe(false);
 		expect(isObject(undefined)).toBe(false);
 		expect(isObject(null)).toBe(false);
