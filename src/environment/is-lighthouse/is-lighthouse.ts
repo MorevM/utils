0273f0648e3cd8a9dfd001d8b78f7fd78d8e4fb4
MorevM <max.seainside@gmail.com>
2024-11-14 22:08:11 +0300

    feat(environment): Deprecate `isLighthouse` function

--- src/environment/is-lighthouse/is-lighthouse.ts
+++ src/environment/is-lighthouse/is-lighthouse.ts
@@ -1,16 +1,22 @@
+import { isClient } from '../is-client/is-client';
+
 /**
  * Checks if a Lighthouse user agent is being used. \
  * I condemn the use, but it is difficult to explain to customers.
  *
- * @returns   Whether the browser with Lighthouse user agent is being used.
+ * @deprecated
+ * `Lighthouse' does not include this data in `userAgent' since January 26, 2023. \
+ * The function is now useless - it will always return `false`.
+ * Related PR: https://github.com/GoogleChrome/lighthouse/pull/14384
+ *
+ * @param   userAgent   `userAgent` header.
+ *
+ * @returns             Whether the browser with Lighthouse user agent is being used.
  */
-
-import { isClient } from '../is-client/is-client';
-
 export const isLighthouse = (userAgent?: string): boolean => {
 	if (!userAgent && isClient()) {
 		userAgent = window.navigator.userAgent;
 	}
 	return ['Lighthouse', 'Page Speed', 'PageSpeed']
-		.some(part => (userAgent ?? '').includes(part));
+		.some((part) => (userAgent ?? '').includes(part));
 };
