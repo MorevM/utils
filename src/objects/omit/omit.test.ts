43fd07b83912bdcf104ae6c56af05e3b63c34f7a
MorevM <max.seainside@gmail.com>
2025-05-30 22:45:19 +0300

    feat(objects): Add `omitMutable` utility

--- src/objects/omit/omit.test.ts
+++ src/objects/omit/omit.test.ts
@@ -1,6 +1,16 @@
-import { omit } from './omit';
+import { omit, omitMutable } from './omit';
 
 describe('omit', () => {
+	it('Returns empty object in case of nullish input', () => {
+		/* @ts-expect-error -- Edge case */
+		const nullResult = omit(null, 'foo', 'bar');
+		/* @ts-expect-error -- Edge case */
+		const undefinedResult = omit(undefined, 'foo', 'bar');
+
+		expect(nullResult).toStrictEqual({});
+		expect(undefinedResult).toStrictEqual({});
+	});
+
 	it('Returns the object which is a given object without a given keys', () => {
 		const input = {
 			foo: 1,
@@ -57,5 +67,83 @@ describe('omit', () => {
 		const result = omit(input, 'bar', 'some');
 
 		expect(result).toStrictEqual(expected);
+	});
+});
+
+describe('omit-mutable', () => {
+	it('Returns input as is in case of nullish input', () => {
+		/* @ts-expect-error -- Edge case */
+		const nullResult = omitMutable(null, 'foo', 'bar');
+		/* @ts-expect-error -- Edge case */
+		const undefinedResult = omitMutable(undefined, 'foo', 'bar');
+
+		expect(nullResult).toBeNull();
+		expect(undefinedResult).toBeUndefined();
+	});
+
+	it('Mutates and returns the object without given keys', () => {
+		const input = {
+			foo: 1,
+			bar: 2,
+			baz: 3,
+		};
+		const expected = {
+			bar: 2,
+		};
+
+		const result = omitMutable(input, 'foo', 'baz');
+
+		expect(result).toStrictEqual(expected);
+		expect(input).toStrictEqual(expected);
+		expect(result).toBe(input);
+	});
+
+	it('Returns a given object as is if no keys to remove given', () => {
+		const input = {
+			foo: 1,
+			bar: 2,
+			baz: 3,
+		};
+		const expected = { ...input };
+
+		const result = omitMutable(input);
+
+		expect(result).toStrictEqual(expected);
+		expect(input).toStrictEqual(expected);
+		expect(result).toBe(input);
+	});
+
+	it('Returns empty object if all keys of the input object given', () => {
+		const input = {
+			foo: 1,
+			bar: 2,
+			baz: 3,
+		};
+		const expected = {};
+
+		const result = omitMutable(input, 'foo', 'bar', 'baz');
+
+		expect(result).toStrictEqual(expected);
+		expect(input).toStrictEqual(expected);
+		expect(result).toBe(input);
+	});
+
+	it('Works correctly if some of a given keys does not exists in a given object', () => {
+		const input = {
+			foo: 1,
+			bar: 2,
+			baz: 3,
+		};
+		const expected = {
+			foo: 1,
+			baz: 3,
+		};
+
+		/* @ts-expect-error -- Edge case */
+		const result = omitMutable(input, 'bar', 'some');
+
+		expect(result).toStrictEqual(expected);
+		expect(input).toStrictEqual(expected);
+		expect(result).toBe(input);
 	});
 });
--- src/objects/omit/omit.ts
+++ src/objects/omit/omit.ts
@@ -1,10 +1,31 @@
+import { isNullish } from '../../guards';
+
 /**
  * Removes the specified keys from the given object and returns new object.
  *
  * @param   obj    Original object.
  * @param   keys   Keys of original object to remove.
  *
- * @returns        New object without specified keys
+ * @returns        New object without specified keys.
  */
 export const omit = <T, K extends keyof T>(obj: T, ...keys: K[]): Omit<T, K> =>
 	Object.fromEntries(Object.entries(obj ?? {}).filter(([k]) => !keys.includes(k as K))) as Omit<T, K>;
+
+/**
+ * Removes the specified keys from the given object and returns the same object.
+ * **MUTATES** the original object.
+ *
+ * @param   obj    Original object.
+ * @param   keys   Keys of original object to remove.
+ *
+ * @returns        The mutated object without specified keys.
+ */
+export const omitMutable = <T, K extends keyof T>(obj: T, ...keys: K[]): Omit<T, K> => {
+	if (isNullish(obj)) return obj;
+
+	for (const key of keys) {
+		delete obj[key];
+	}
+
+	return obj as Omit<T, K>;
+};
