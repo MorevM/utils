0579b4f644b70e4d67db31f22c8245a47c4a6dab
MorevM <max.seainside@gmail.com>
2026-03-23 22:47:59 +0300

    refactor(guards): Deprecate `isNumeric` utility function

--- src/guards/is-numeric/is-numeric.test.ts
+++ src/guards/is-numeric/is-numeric.test.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-deprecated, import-x/no-deprecated */
 import { isNumeric } from './is-numeric';
 
-describe('is-numeric', () => {
+describe(isNumeric, () => {
 	it('Returns `true` if a given value is an integer number', () => {
 		expect(isNumeric(1)).toBe(true);
 	});
@@ -11,6 +12,11 @@ describe('is-numeric', () => {
 
 	it('Returns `false` if a given value is `NaN`', () => {
 		expect(isNumeric(NaN)).toBe(false);
+	});
+
+	it('Returns `true` if a given value is `Infinity`', () => {
+		expect(isNumeric(Infinity)).toBe(true);
+		expect(isNumeric(-Infinity)).toBe(true);
 	});
 
 	it('Returns `false` if a given value is not a number', () => {
--- src/guards/is-numeric/is-numeric.ts
+++ src/guards/is-numeric/is-numeric.ts
@@ -1,6 +1,10 @@
 /**
  * Checks whether a given value is a valid number.
  *
+ * @deprecated
+ * This function will be removed in the next major release. \
+ * Use `isNumber` or `isFiniteNumber` instead depending on desired semantics.
+ *
  * @param   value   The value being evaluated.
  *
  * @returns         Whether the value is a valid number
